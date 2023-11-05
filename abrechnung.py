bc444226aedfffb36129f96ed80a34b9a23d3ab4
Martin Herrmann <martin@famherrmann.de>
2023-11-05 23:06:38 +0100

    Use pydantic

--- abrechnung.py
+++ abrechnung.py
@@ -4,8 +4,10 @@ from pathlib import Path
 import re
 from datetime import datetime
 
+from pydantic import BaseModel, conlist, Field
 import pypdf as pdf
 import typer
+
 
 WEEKDAYS = {
     "mon": calendar.MONDAY,
@@ -16,6 +18,39 @@ WEEKDAYS = {
     "sat": calendar.SATURDAY,
     "sun": calendar.SUNDAY,
 }
+
+
+class Trainer(BaseModel):
+    name: str
+    address: conlist(str, min_length = 1, max_length = 2)
+    iban: str
+    hourly_fee: float
+
+
+class TimeRange(BaseModel):
+    start: str
+    end: str
+
+    def hours(self):
+        return (datetime.strptime(self.end, "%H:%M") - datetime.strptime(self.start, "%H:%M")).total_seconds() / 3600
+
+
+class Class(BaseModel):
+    name: str
+    weekday: str
+    time: TimeRange
+
+
+class Template(BaseModel):
+    file: str
+    fee_column_prefix: str
+    total_fee_column: str
+
+
+class Data(BaseModel):
+    trainer: Trainer
+    class_: Class = Field(alias = "class")
+    template: Template
 
 
 def days(year: int, month: int, weekday: int) -> list[int]:
@@ -36,9 +71,10 @@ def format_number(value: float) -> str:
 
 def abrechnung(data_file: Path, year: int, month: int, participant_count: list[int]):
     with open(data_file, "rb") as f:
-        data = tomllib.load(f)
+        doc = tomllib.load(f)
+        data = Data.model_validate(doc)
 
-    input_file = data_file.parent / data["template"]["file"]
+    input_file = data_file.parent / data.template.file
 
     print(f"Reading {input_file}")
     reader = pdf.PdfReader(open(input_file, "rb"), strict=False)  # TODO strict?
@@ -46,58 +82,43 @@ def abrechnung(data_file: Path, year: int, month: int, participant_count: list[i
     writer = pdf.PdfWriter()
     writer.add_page(reader.pages[0])
 
-    writer.update_page_form_field_values(
-        writer.pages[0], {"Monat": "Max Mustermann"}
-    )
-
-    trainer_name = data["trainer"]["name"]
-    class_name = data["class"]["name"]
-
     writer.update_page_form_field_values(writer.pages[0], {
-        "Monat": trainer_name,
-        "1": data["trainer"]["address"][0],
-        "2": data["trainer"]["address"][1],
-        "3": data["trainer"]["iban"],
+        "Monat": data.trainer.name,
+        "1": data.trainer.address[0],
+        "2": data.trainer.address[1],
+        "3": data.trainer.iban,
     })
 
     writer.update_page_form_field_values(writer.pages[0], {
-        "sportart": class_name,  # Sportart
+        "sportart": data.class_.name,  # Sportart
         "undefined": f"{month}/{year}",  # Monat
     })
 
-    start_time = datetime.strptime(data['class']['time']['start'], "%H:%M")
-    end_time = datetime.strptime(data['class']['time']['end'], "%H:%M")
-
-    duration = end_time - start_time
-    hourly_fee = data["trainer"]["hourly_fee"]
-
-    weekday = data["class"]["weekday"]
     total_hours = 0
     total_fee = 0
 
-    ds = days(year, month, WEEKDAYS[weekday.lower()])
+    ds = days(year, month, WEEKDAYS[data.class_.weekday.lower()])
     for i, (day, count) in enumerate((day, count) for day, count in zip(ds, participant_count) if count > 0):
-        hours = duration.total_seconds() / 3600
-        fee = hourly_fee * hours
+        fee = data.trainer.hourly_fee * data.class_.time.hours()
 
         writer.update_page_form_field_values(writer.pages[0], {
             f"DatumRow{i+1}": f"{day}.{month}.{year}",
-            f"ArbeitszeitRow{i+1}": f"{data['class']['time']['start']} - {data['class']['time']['end']}",
-            f"StdRow{i+1}": format_number(duration.total_seconds() / 3600),
-            f"{data['template']['fee_column_prefix']}{i + 1}": format_number(fee),
+            f"ArbeitszeitRow{i+1}": f"{data.class_.time.start} - {data.class_.time.end}",
+            f"StdRow{i+1}": format_number(data.class_.time.hours()),
+            f"{data.template.fee_column_prefix}{i + 1}": format_number(fee),
             f"Teil nehmerRow{i + 1}": count,
         })
 
-        total_hours += hours
+        total_hours += data.class_.time.hours()
         total_fee += fee
 
     writer.update_page_form_field_values(writer.pages[0], {
         "summe": format_number(total_hours),
-        data['template']['total_fee_column']: format_number(total_fee),
+        data.template.total_fee_column: format_number(total_fee),
         "Braunschweig den": datetime.today().strftime("%d.%m.%Y"),
     })
 
-    output_file_name = f"Trainerabrechnung {trainer_name} {class_name} {year}-{month:02d}.pdf"
+    output_file_name = f"Trainerabrechnung {data.trainer.name} {data.class_.name} {year}-{month:02d}.pdf"
     output_file = data_file.with_name(output_file_name)
     print(f"Writing {output_file}")
     with open(output_file, "wb") as f:
